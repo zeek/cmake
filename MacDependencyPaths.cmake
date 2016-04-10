5c4b2030638a4417df102b12971fc1daee97c1f9
Vlad Grigorescu <grigorescu@gmail.com>
2016-04-10 16:52:31 -0500

    Check for ports, brew and fink, and add the prefixes as needed.

--- MacDependencyPaths.cmake
+++ MacDependencyPaths.cmake
@@ -2,8 +2,21 @@
 # default search prefix paths, but the nicer thing would be if they are
 # prepended to the default, so that is fixed here.
 if (APPLE AND "${PROJECT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
-    list(INSERT CMAKE_PREFIX_PATH 0 /opt/local) # MacPorts
-    list(INSERT CMAKE_PREFIX_PATH 0 /usr/local) # Homebrew, if linked
-    list(INSERT CMAKE_PREFIX_PATH 0 /usr/local/opt/openssl) # Homebrew OpenSSL
-    list(INSERT CMAKE_PREFIX_PATH 0 /sw)        # Fink
+    find_program(MAC_PORTS_BIN ports)
+    find_program(MAC_HBREW_BIN brew)
+    find_program(MAC_FINK_BIN fink)
+
+    if (MAC_PORTS_BIN)
+      list(INSERT CMAKE_PREFIX_PATH 0 ${MAC_PORTS_BIN}) # MacPorts
+    endif ()
+    
+    if (MAC_HBREW_BIN)
+        list(INSERT CMAKE_PREFIX_PATH 0 /usr/local) # Homebrew, if linked
+        list(INSERT CMAKE_PREFIX_PATH 0 /usr/local/opt/openssl) # Homebrew OpenSSL
+    endif ()
+
+    if (MAC_FINK_BIN)
+       list(INSERT CMAKE_PREFIX_PATH 0 /sw)        # Fink
+    endif ()
+    
 endif ()
