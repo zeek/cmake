8554b602eed13076484fdac18fbdd934b061bed7
Jon Siwek <jsiwek@corelight.com>
2019-04-17 15:55:23 -0700

    Configure rpath for multiple library install dirs
    E.g. projects may use either lib/ or lib64/

--- SetupRPATH.cmake
+++ SetupRPATH.cmake
@@ -4,4 +4,13 @@
 if (NOT BINARY_PACKAGING_MODE)
     SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
     SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+
+    include(GNUInstallDirs)
+
+    if ( NOT CMAKE_INSTALL_LIBDIR STREQUAL "lib" )
+        # Ideally, we'd consistently use just one lib dir (e.g. lib/ or lib64/),
+        # which requires every sub-project and external/embedded dependency
+        # agrees and/or offers ability to install at that canonical location.
+        set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+    endif ()
 endif ()
