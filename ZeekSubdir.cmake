1c69d96e1b5445c99b53c41f37080ef2907fd4dc
Dominik Charousset <dominik@charousset.de>
2019-10-31 09:38:42 +0100

    Use object libraries for subdir libs and plugins

--- ZeekSubdir.cmake
+++ ZeekSubdir.cmake
@@ -2,14 +2,8 @@
 # Creates a target for a library of objects file in a subdirectory,
 # and adds to the global bro_SUBDIR_LIBS.
 function(bro_add_subdir_library name)
-    if ( bro_HAVE_OBJECT_LIBRARIES )
-        add_library("bro_${name}" OBJECT ${ARGN})
-        set(_target "$<TARGET_OBJECTS:bro_${name}>")
-    else ()
-        add_library("bro_${name}" STATIC ${ARGN})
-        set(_target "bro_${name}")
-    endif ()
-
-    set(bro_SUBDIR_LIBS "${_target}" ${bro_SUBDIR_LIBS} CACHE INTERNAL "subdir libraries")
+    add_library("bro_${name}" OBJECT ${ARGN})
+    set(bro_SUBDIR_LIBS "$<TARGET_OBJECTS:bro_${name}>" ${bro_SUBDIR_LIBS} CACHE INTERNAL "subdir libraries")
+    set(bro_SUBDIR_DEPS "bro_${name}" ${bro_SUBDIR_DEPS} CACHE INTERNAL "subdir dependencies")
     add_clang_tidy_files(${ARGN})
 endfunction()
