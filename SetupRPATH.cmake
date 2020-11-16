0c56404a569b2e2c776f820a2fd3e496d787c208
Christian Kreibich <christian@corelight.com>
2020-11-16 15:11:45 -0800

    Make RPATH setup rely on GNUInstallDirs

--- SetupRPATH.cmake
+++ SetupRPATH.cmake
@@ -2,15 +2,8 @@
 # can find internal/private libraries or libraries external to the build
 # directory that were explicitly linked against
 if (NOT BINARY_PACKAGING_MODE)
-    SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-    SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-
     include(GNUInstallDirs)
 
-    if ( NOT CMAKE_INSTALL_LIBDIR STREQUAL "lib" )
-        # Ideally, we'd consistently use just one lib dir (e.g. lib/ or lib64/),
-        # which requires every sub-project and external/embedded dependency
-        # agrees and/or offers ability to install at that canonical location.
-        set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_RPATH};${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
-    endif ()
+    SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
 endif ()
