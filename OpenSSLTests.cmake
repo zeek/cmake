a0b67ffacde9721dbbccd25feda2adcbd8b76397
Johanna Amann <johanna@corelight.com>
2021-11-17 14:53:54 +0000

    Older cmake versions do not set OPENSSL_VERSION for OpenSSL 3.0
    Let's sidestep that by just ignoring that it does not exist in these
    cases...

--- OpenSSLTests.cmake
+++ OpenSSLTests.cmake
@@ -31,8 +31,10 @@ if (NOT including_ssl_h_works)
     endif ()
 endif ()
 
-if (OPENSSL_VERSION VERSION_LESS "0.9.7")
-    message(FATAL_ERROR "OpenSSL >= v0.9.7 required")
+if (DEFINED OPENSSL_VERSION)
+    if (OPENSSL_VERSION VERSION_LESS "0.9.7")
+        message(FATAL_ERROR "OpenSSL >= v0.9.7 required")
+    endif()
 endif ()
 
 check_include_files(openssl/kdf.h OPENSSL_HAVE_KDF_H)
