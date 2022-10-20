bea2f168555e13010b11b5ff0942d7650e2c9a45
Tim Wojtulewicz <tim@corelight.com>
2022-10-20 08:40:14 -0700

    Remove tests for LIBPCAP_PCAP_COMPILE_NOPCAP_HAS_ERROR_PARAMETER

--- PCAPTests.cmake
+++ PCAPTests.cmake
@@ -27,52 +27,6 @@ if (NOT HAVE_LIBPCAP_PCAP_FREECODE)
     message(STATUS "No implementation for pcap_freecode()")
 endif ()
 
-check_c_source_compiles("
-#include <pcap.h>
-int main () {
-    int snaplen;
-    int linktype;
-    struct bpf_program fp;
-    int optimize;
-    bpf_u_int32 netmask;
-    char str[10];
-    char error[1024];
-    snaplen = 50;
-    linktype = DLT_EN10MB;
-    optimize = 1;
-    netmask = 0L;
-    str[0] = 'i'; str[1] = 'p'; str[2] = '\\\\0';
-    (void)pcap_compile_nopcap(
-        snaplen, linktype, &fp, str, optimize, netmask, &error);
-    return 0;
-}
-" LIBPCAP_PCAP_COMPILE_NOPCAP_HAS_ERROR_PARAMETER)
-if (NOT LIBPCAP_PCAP_COMPILE_NOPCAP_HAS_ERROR_PARAMETER)
-    # double check
-    check_c_source_compiles("
-#include <pcap.h>
-int main () {
-    int snaplen;
-    int linktype;
-    struct bpf_program fp;
-    int optimize;
-    bpf_u_int32 netmask;
-    char str[10];
-    snaplen = 50;
-    linktype = DLT_EN10MB;
-    optimize = 1;
-    netmask = 0L;
-    str[0] = 'i'; str[1] = 'p'; str[2] = '\\\\0';
-    (void)pcap_compile_nopcap(snaplen, linktype, &fp, str, optimize, netmask);
-    return 0;
-}
-" LIBPCAP_PCAP_COMPILE_NOPCAP_NO_ERROR_PARAMETER)
-    if (NOT LIBPCAP_PCAP_COMPILE_NOPCAP_NO_ERROR_PARAMETER)
-        message(FATAL_ERROR
-            "Can't determine if pcap_compile_nopcap takes an error parameter")
-    endif ()
-endif ()
-
 check_symbol_exists(DLT_PPP_SERIAL pcap.h HAVE_DLT_PPP_SERIAL)
 if (NOT HAVE_DLT_PPP_SERIAL)
     set(DLT_PPP_SERIAL 50)
